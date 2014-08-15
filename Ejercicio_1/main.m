5c613d7db32afa50b3f3f3ee217da2aa72e9b162
CCDM19 <LABORATORIOMAC@iMac-de-CCDM19.local>
2014-08-15 10:09:09 -0600

    Initial Commit

---
+++ Ejercicio_1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ejercicio_1
+//
+//  Created by CCDM19 on 15/08/14.
+//  Copyright (c) 2014 N. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
